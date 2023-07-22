a80869dc25c2371bcfcc7d65068987cd8b5c44d0
Ibraaaheeeem <ibraaaheeeem@gmail.com>
2023-07-22 09:36:00 -0700

    Delete duplicate method

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -7,17 +7,8 @@ Starts a Flask web application
 from flask import Flask
 app = Flask(__name__)
 
-
-@app.route('/', strict_slashes=False)
-def index():
-    """
-    returns Hello HBNB!
-
-    """
-    return 'Hello HBNB!'
-
 @app.route('/hbnb', strict_slashes=False)
-def index():
+def hbnb():
     """
     returns HBNB
 
